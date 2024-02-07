83c8fdcecac39e8dc5d5294f883128cc1ab45eb9
Tymek <tdomagala@student.agh.edu.pl>
2024-02-07 13:36:36 +0100

    Added basic setup of SFML

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
+#define SFML_STATIC
+
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(void)
 {
-    std::cout << "Hello World!" << std::endl;
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Pathfinding Algorithms");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here
+        
+
+        // end the current frame
+        window.display();
+    }
+
     return 0;
 }
